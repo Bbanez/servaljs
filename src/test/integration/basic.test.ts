b08a37df58d0d38190d6a014a5558cd1be592102
bbanez <banezprod@gmail.com>
2023-06-21 17:59:25 +0200

    Bump versio to 0.1.2

--- src/test/integration/basic.test.ts
+++ src/test/integration/basic.test.ts
@@ -1,12 +1,39 @@
 import { HttpStatus } from '@/http-error';
+import { createLogger } from '@/index';
 import { createServal } from '@/main';
-import { createController, createControllerMethod } from '@/rest';
+import {
+  createController,
+  createControllerMethod,
+  createMiddleware,
+} from '@/rest';
 
 async function main() {
   createServal({
     server: {
       port: 1280,
     },
+    modules: [
+      createLogger({
+        saveToFile: {
+          interval: 1000,
+          output: 'logs',
+        },
+      }),
+    ],
+    middleware: [
+      createMiddleware({
+        name: 'Mid',
+        path: '/test',
+        handler() {
+          return (_req, res) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify({ test: true }));
+            res.end();
+          };
+        },
+      }),
+    ],
     controllers: [
       createController({
         name: 'Test',
